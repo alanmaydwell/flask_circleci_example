9fe09ad5a3c4c90200debf95ab52ee3bc2233b26
alanmaydwell <alan.maydwell@ntlworldo.com>
2021-12-24 14:25:37 +0000

    added more to flask

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello Earthling!"
+def home():
+    return render_template('index.html')
+
+# Dynamic !
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('hello.html', name=name)
 
